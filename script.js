abcdc331b47c50cb143c24f1443f6fc5bf829cbc
orozobek-omurkulov <76588066+orozobek-omurkulov@users.noreply.github.com>
2021-01-29 20:23:45 +0600

    Uploaded files

---
+++ script.js
@@ -0,0 +1,28 @@
+
+const createItem = (todo, id) => {
+
+    let item = `
+        <li
+            key="${id}"
+            class=${todo.checked ? 'taskItem-done' : 'taskItem'}>
+                ${todo.title}${todo.date ? todo.date :''}
+                <img class="btn" src="check-icon.png" onclick="completeTodo(event)"></img>
+                <img class="btn" src="delete-icon.png" onclick="deleteTodo(event)"></img>
+        </li>
+    `
+    return item
+
+}
+
+const renderTodoItems = () => {
+    list.innerHTML = ''
+    let items = []
+
+    todosArray.length
+    ?   todosArray.map((todo, id) => {
+        items.unshift(createItem(todo, id))
+        list.innerHTML = items.join('')
+    })
+    : list.prepend ('There is no task')
+}
+renderTodoItems()
